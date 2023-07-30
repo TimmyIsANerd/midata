8a25a6161930301031ae2665011753c56a453353
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-07-30 01:06:20 +0100

    update: added new attributes

--- api/models/User.js
+++ api/models/User.js
@@ -6,31 +6,40 @@
  */
 
 module.exports = {
-
   attributes: {
-    emailAddress:{
-      type:'string',
-      isEmail:true,
-      description:"The email address for this user.",
-      unique:true,
+    emailAddress: {
+      type: "string",
+      isEmail: true,
+      description: "The email address for this user.",
+      unique: true,
     },
-    phoneNumber:{
-      type:"string",
-      description:"The phone number for this user."
+    password: {
+      type: "string",
+      description: "The password for this user.",
     },
-    password:{
-      type:'string',
-      description:"The password for this user.",
+    firstName: {
+      type: "string",
+      description: "The first name for this user.",
     },
-    firstName:{
-      type:'string',
-      description:"The first name for this user.",
+    lastName: {
+      type: "string",
+      description: "The last name for this user.",
     },
-    lastName:{
-      type:'string',
-      description:"The last name for this user.",
-    }
+    tosIp: {
+      type: "string",
+      description: "IP Address that accepted Terms of Service",
+    },
+    emailVerificationStatus: {
+      type: "string",
+      description: "Email Verification Status, Unverified/Verified",
+      isIn: ["Verified", "Unverified"],
+      defaultsTo: "Unverified",
+    },
+    isAdmin: {
+      type: "boolean",
+      description: "Whether this user is an admin or not",
+      extendedDescription: "If true, this user can access the admin panel.",
+      defaultsTo: false,
+    },
   },
-
 };
-
