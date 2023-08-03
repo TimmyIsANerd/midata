af306c37c943511310dfc174ba50973010627695
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-03 23:05:12 +0100

    add: logic to process paystack webhook

---
+++ api/controllers/webhook/paystack.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Paystack',
+
+
+  description: 'Paystack webhook.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // @todo 
+
+  }
+
+
+};
