9202e8e59e7a899725ec3869b17bf85032f777e0
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-08-14 02:45:51 +0100

    add: logic to create monitor

---
+++ api/controllers/admin/user/create.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+
+  friendlyName: 'Create Monitor',
+
+
+  description: 'Create Monitor User linked to admin.',
+
+
+  inputs: {
+    emailAddress:{
+      type:'string',
+      isEmail:true
+    }
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
